e1fa2f5ebb7933d6a7f0f202374d1341003a1910
LHarveyDev <>
2023-12-31 19:42:09 +0000

    Fix navbar not returning user to homepage

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import os
-from flask import Flask, render_template
-
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
@@ -22,6 +32,6 @@ def register():
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
         debug=True)
